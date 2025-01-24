172e79b6cf151d9077cbf8fdd6aa203abefe2ae8
dtonal <dtonal@posteo.de>
2025-01-24 16:55:13 +0700

    create redner class and read more details

--- reader/reader.py
+++ reader/reader.py
@@ -1,4 +1,5 @@
 from lxml import etree
+from .redner import Redner, Name, Rolle
 
 # Paths to the XML and DTD files
 xml_file = "reader/testdata/20208.xml"
@@ -23,14 +24,31 @@ try:
     for rede in root.findall(".//rede"):
         rede_id = rede.get("id")  # Get the "id" attribute of the "rede" element
         # Find the "name" element inside the "redner" tag
-        redner = rede.find(".//redner/name")
+        redner = rede.find(".//redner")
         if redner is not None:
             # Extract the speaker's full name
-            vorname = redner.find("vorname").text if redner.find("vorname") is not None else ""
-            nachname = redner.find("nachname").text if redner.find("nachname") is not None else ""
+            redner_id = redner.get("id")
+            redner_name = rede.find(".//redner/name")
+            vorname = redner_name.find("vorname").text if redner_name.find("vorname") is not None else ""
+            nachname = redner_name.find("nachname").text if redner_name.find("nachname") is not None else ""
+            fraktion = redner_name.find("fraktion").text if redner_name.find("fraktion") is not None else ""
             full_name = f"{vorname} {nachname}"
         else:
             full_name = "Unknown Speaker"
-        print(f"Rede ID: {rede_id}, Speaker: {full_name}")
+        print(f"Rede ID: {rede_id}, Speaker: {full_name} [{redner_id}] ({fraktion})")
+        print("\n")
+        # Iterate through all the paragraphs within the "rede" element
+        # for paragraph in rede.findall("p"):
+        #     # Check if the "klasse" attribute is "J", "J_1", or "O"
+        #     klasse = paragraph.get("klasse")
+        #     if klasse in ["J", "J_1", "O"]:
+        #         # Extract the text of each paragraph (ignoring extra whitespace)
+        #         paragraph_text = paragraph.text.strip() if paragraph.text else ""
+                
+        #         # Print each paragraph's text
+        #         if paragraph_text:
+        #             print(paragraph_text)
+        # print("\n")
+
 except Exception as e:
     print(f"Error reading or validating XML: {e}")
